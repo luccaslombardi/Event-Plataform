15a0bfe957ef59ba0c84de2ef4e9e3813f995caf
Dev Sweeteez <itech@sweeteez.com.br>
2022-06-24 12:52:35 -0300

    finalização do projeto

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,7 +26,6 @@ interface GetLessonsQueryResponse {
 export function Sidebar() {
   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
 
-  console.log(data);
 
   return (
     <aside className="w-[384px] bg-gray-700 p-6 border-l border-gray-600">
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4plgxix1u9m01xx7c2xg6yo/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization':`Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
